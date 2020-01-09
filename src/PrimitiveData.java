8f474a500804d509368ba046d0635883d52a9a42
Lisa <lisa.li72159@gmail.com>
2020-01-08 19:21:47 -0500

    Primitive Data

---
+++ src/PrimitiveData.java
@@ -0,0 +1,28 @@
+public class PrimitiveData {
+
+
+        public static void main(String[] args) {
+            int myValue = 1000;
+
+            int myMinIntValue = Integer.MIN_VALUE;
+            int myMaxIntValue = Integer.MAX_VALUE;
+            System.out.println("Integer Minimum Value = " + myMinIntValue);
+            System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
+            byte myMinByteValue = Byte.MIN_VALUE;
+            byte myMaxByteValue = Byte.MAX_VALUE;
+            System.out.println("Byte Minimum Value " + myMinByteValue);
+            System.out.println("Byte Maximum Value " + myMaxByteValue);
+
+            short myMinshortValue = Short.MIN_VALUE;
+            short myMaxshortValue = Short.MAX_VALUE;
+            System.out.println("Short Minimum Value " + myMinshortValue);
+            System.out.println("Short Maximum Value " + myMaxshortValue);
+
+            long myMinlongValue = Long.MIN_VALUE;
+            long myMaxlongValue = Long.MAX_VALUE;
+            System.out.println("Long Minimum Value " + myMinlongValue);
+            System.out.println("Long Maximum Value " + myMaxlongValue);
+
+        }
+}
